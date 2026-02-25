4224a9456d52040a52c6a77b898512091968856e
Josephquito <jdquito.3@sudamericano.edu.ec>
2026-02-25 18:33:41 -0500

    home2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
-  darkMode: false, // Esto desactiva el modo oscuro basado en media queries
+  darkMode: "class", // 👈 mejor práctica actual
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["light"], // 👈 Esto fuerza el tema claro
+    themes: ["light"], // fuerza solo tema claro
   },
 };
